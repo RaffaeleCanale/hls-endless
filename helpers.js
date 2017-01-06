169c8c083fdd091579fcb5b9a33987d8667f8a18
RaffaeleCanale <raffaele.canale@livestream.com>
2017-01-06 17:18:38 -0500

    Add library.js

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 module.exports = {
-  maxSegmentsInPlaylist:5,
+  maxSegmentsInPlaylist:10,
   getSegmentsDuration: function(file){
     var data = fs.readFileSync(file, 'utf-8');
     var re = /EXTINF:([\d\.]+),/gi;
@@ -9,6 +9,15 @@ module.exports = {
       segments.push(found[1]);
       duration+=parseFloat(found[1]);
     }
-    return { totalDuration:duration, segments: segments};
+
+    re = /(.+\.ts)/gi;
+    var names = []
+    while (found = re.exec(data)){
+      names.push(found[1]);
+    }
+
+    if (names.length !== segments.length) throw new Error('Invalid m3u8 file')
+
+    return { totalDuration:duration, segments, names};
   }
 }
---
+++ library.js
@@ -0,0 +1,43 @@
+var helpers = require('./helpers');
+
+module.exports = (m3u8Path) => {
+
+    var startTime = Date.now();
+    var gear1 = m3u8Path;
+    // gear1 = __dirname + '/../p2p-live-cdn-cache/cache/http___qthttp_apple_com_edgesuite_net_1010qwoeiuryfg_1240_vod_m3u8'
+    var playlistInfo = helpers.getSegmentsDuration(gear1);
+
+    return {
+        getPlayList: (id) => {
+            // var id = parseInt(req.param('id'),10);
+            if (isNaN(id) || id > 4) {
+                throw new Error('id should be in [0,4]')
+            }
+            var now = Date.now();
+            var delta = (now - startTime) / 1000;
+            var totalLoops = Math.floor(delta / playlistInfo.totalDuration);
+            var deltaInLoop = delta - (playlistInfo.totalDuration * totalLoops);
+            var sequence = totalLoops * playlistInfo.segments.length;
+            var currentSegment = 0;
+            var segments = [];
+            var m3u8Template, videoPath, _segment;
+            for (var i in playlistInfo.segments) {
+                _segment = playlistInfo.segments[i];
+                deltaInLoop -= _segment;
+                if (deltaInLoop > 0) {
+                    currentSegment++;
+                } else if (segments.length < helpers.maxSegmentsInPlaylist) {
+                    // videoPath = ",\n/bipbop_4x3/gear" + id + "/segment_" + i + (id != 0 ? ".ts" : ".aac");
+                    videoPath = ',\n' + playlistInfo.names[i];
+                    segments.push("#EXTINF:" + _segment + videoPath);
+                } else {
+                    break;
+                }
+            }
+            m3u8Template = "#EXTM3U\n#EXT-X-TARGETDURATION:11\n#EXT-X-VERSION:3\n#EXT-X-MEDIA-SEQUENCE:" + (sequence + currentSegment);
+            m3u8Template += "\n" + segments.join('\n') + "\n";
+
+            return m3u8Template
+        }
+    }
+}
